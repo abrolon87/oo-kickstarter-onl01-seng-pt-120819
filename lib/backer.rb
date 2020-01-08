f5f2f69157c5251b1fbc237ca6d087ed2f18d0b6
Amanda Rolon <abrolon87@gmail.com>
2020-01-08 16:55:30 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,19 +1,11 @@
 class Backer
   attr_reader :name, :backed_projects
-  @backed_projects = []
+
 
   def initialize(name)
     @name = name
-    
+    @backed_projects = []
   end
 
-  def backed_projects
-    pbs = ProjectBacker.all.select {|pb| pb.backer == self}
-    pbs.map {|pb| pb.project}
-  end
-
-  def back_project(project)
-    ProjectBacker.new(project, self)
-  end
 
 end
